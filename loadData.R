b0d759d68e32356dea39aa52bf3e5870a8ee85a6
pbiecek <przemyslaw.biecek@gmail.com>
2013-06-24 16:19:13 +0200

    scored items data with updated loader

--- loadData.R
+++ loadData.R
@@ -1,4 +1,3 @@
-setwd("c:/_Przemek_/PISA2009")
 setwd("/Users/pbiecek/camtasia/PISA2003/")
 
 library(devtools)
@@ -7,42 +6,26 @@ install_github('PISAtools', 'pbiecek')
 library(PISAtools)
 
 # School
-school2009 <- extract.data('INT_SCQ09_Dec11.txt', 'INT_SCQ09_SAS_DEC11.sas')
-save(school2009 , file="school2009.rda")
+school2003 <- extract.data('INT_schi_2003.txt', 'Read_schI_2003.sas')
+save(school2003 , file="school2003.rda")
 
-school2009dict <- extract.dict("INT_SCQ09_SAS_DEC11.sas")
-school2009dict$colnames <-  extract.vardict("INT_SCQ09_SAS_DEC11.sas")
-save(school2009dict , file="school2009dict.rda")
+school2003dict <- extract.dict("Read_schI_2003.sas")
+school2003dict$colnames <-  extract.vardict("Read_schI_2003.sas")
+save(school2003dict , file="school2003dict.rda")
 
 # Scored items
-scoredItem2009 <- extract.data("INT_COG09_S_DEC11.txt", "INT_COG09_S_SAS_DEC11.sas")
-save(scoredItem2009, file="scoredItem2009.rda")
+scoredItem2003 <- extract.data("INT_cogn_2003.txt", "Read_cogn_2003.sas")
+save(scoredItem2003, file="scoredItem2003.rda")
 
-scoredItem2009dict <- extract.dict("INT_COG09_S_SAS_DEC11.sas")
-scoredItem2009dict$colnames <-  extract.vardict("INT_COG09_S_SAS_DEC11.sas")
-save(scoredItem2009dict , file="scoredItem2009dict.rda")
-
-# items
-item2009 <- extract.data("INT_COG09_TD_DEC11.txt", "INT_COG09_TD_SAS_DEC11.sas")
-save(item2009, file="item2009.RData")
-
-item2009dict <- extract.dict("INT_COG09_TD_SAS_DEC11.sas")
-item2009dict$colnames <-  extract.vardict("INT_COG09_TD_SAS_DEC11.sas")
-save(item2009dict , file="item2009dict.rda")
-
-# parent
-parent2009 <- extract.data("INT_PAR09_DEC11.txt", "INT_PAR09_SAS_DEC11.sas")
-save(parent2009, file="parent2009.RData")
-
-parent2009dict <- extract.dict("INT_PAR09_SAS_DEC11.sas")
-parent2009dict$colnames <-  extract.vardict("INT_PAR09_SAS_DEC11.sas")
-save(parent2009dict , file="parent2009dict.rda")
+scoredItem2003dict <- extract.dict("Read_cogn_2003.sas")
+scoredItem2003dict$colnames <-  extract.vardict("Read_cogn_2003.sas")
+save(scoredItem2003dict , file="scoredItem2003dict.rda")
 
 # student
-student2009 <- extract.data("INT_STQ09_DEC11.txt", "INT_STQ09_SAS_DEC11.sas")
-save(student2009, file="student2009.RData")
+student2003 <- extract.data("INT_stui_2003_v2.txt", "Read_stuI_2003_v2.sas")
+save(student2003, file="student2003.RData")
 
-student2009dict <- extract.dict("INT_STQ09_SAS_DEC11.sas")
-student2009dict$colnames <-  extract.vardict("INT_STQ09_SAS_DEC11.sas")
-save(student2009dict , file="student2009dict.rda")
+student2003dict <- extract.dict("Read_stuI_2003_v2.sas")
+student2003dict$colnames <-  extract.vardict("Read_stuI_2003_v2.sas")
+save(student2003dict , file="student2003dict.rda")
 
